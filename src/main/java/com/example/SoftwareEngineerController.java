c8c114893a940c88c190aa8638360170598bd812
Kajuran <Kajuran65@gmail.com>
2025-11-03 12:55:36 -0500

    docker

--- src/main/java/com/example/SoftwareEngineerController.java
+++ src/main/java/com/example/SoftwareEngineerController.java
@@ -1,4 +1,55 @@
 package com.example;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/software-engineers")
 public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers(){
+        return List.of(
+                new SoftwareEngineer(
+                        "js,node,react",
+                        "james",
+                        1
+                        ),
+                new SoftwareEngineer(
+                        "python",
+                        "jack",
+                        2
+
+
+                )
+
+        );
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
